ff2ad1eb7db4f89e0317e0cf6d16995b06afb742
황선혜 <ghkdtjs6451@gmail.com>
2020-07-18 12:13:53 +0900

    http 주소 이미지 가져오기

--- app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        val imageUrl = "https://www.google.com/imgres?imgurl=http%3A%2F%2Fmblogthumb4.phinf.naver.net%2FMjAxNzEyMjJfMTQ5%2FMDAxNTEzOTI5NzgzMjky.FtXbEGsBcQj3hXJIcgWPvySmBIVB4taWRzkxFs4Ap-Ig.U1Ilr5GUV0rzWCywkneLUzVBLyq_HtDWoinUITOEbZQg.JPEG.f4040277%2F9.jpg%3Ftype%3Dw800&imgrefurl=http%3A%2F%2Fm.blog.naver.com%2FPostView.nhn%3FblogId%3Df4040277%26logNo%3D221169069426&tbnid=JEvskg6PJ7vg7M&vet=12ahUKEwj834Kv6NXqAhUFx2EKHZ9iDvoQMygRegUIARDMAQ..i&docid=xB0pkdLL7sTQgM&w=800&h=450&q=%EB%A6%AC%ED%8B%80%EB%9D%BC%EC%9D%B4%EC%96%B8&ved=2ahUKEwj834Kv6NXqAhUFx2EKHZ9iDvoQMygRegUIARDMAQ"
+        val imageUrl = "https://lh3.googleusercontent.com/proxy/lR_uVCo_jm9X87EHg7nOS-LkOoQYMQ3J8zz8ed0PHCj2hy0YYtIFoaZ1lCHW0pUYWdaDGPV2IGVHO6_BmxG6FiYhd05DRYSGhYEsiu9yteen_OdcBuIGJ_zuT0272PggHhmQhYdZfOJo0DHhHH7BVZzZE-sxSikwkVdXTK4DUfrHCQECAOIDRABTHmY6AlLZjOACl1elqVGVDMuPFSPwlyKxcpqizt1yzvqj_xOSiM_hZJiIOc12jPOoH4joJzCyTcQWM66KYvcFIg-6aClxulo27SSN9GbY6Q"
 
         Glide.with(mContext).load(imageUrl).into(profileImg)
 
